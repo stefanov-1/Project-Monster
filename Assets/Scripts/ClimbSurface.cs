b608cd49a4dba05af2c5acc3e491e551866348cd
Soma Nyiro <nysoma@protonmail.com>
2023-05-15 16:14:39 +0200

    added climb surface object with editor script

---
+++ Assets/Scripts/ClimbSurface.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbSurface : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+    public Transform Collision;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
