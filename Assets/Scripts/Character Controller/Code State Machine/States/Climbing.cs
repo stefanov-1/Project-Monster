afabc2b53634f4b6ccf1299d393eb17695419c0e
Soma Nyiro <nysoma@protonmail.com>
2023-05-16 15:46:02 +0200

    made player snap to surface

--- Assets/Scripts/Character Controller/Code State Machine/States/Climbing.cs
+++ Assets/Scripts/Character Controller/Code State Machine/States/Climbing.cs
@@ -27,11 +27,49 @@ public class Climbing : State
     {
         player.rb.useGravity = false;
         player.rb.velocity = Vector3.zero;
+
+        Vector3 closetsPoint = ClosestPointOnLineSegment(
+            ControlValues.Instance.currentClimbStart,
+            ControlValues.Instance.currentClimbEnd,
+            player.rb.position);
+
+        player.rb.position = closetsPoint; // snap the player to the clmbable surface
+
     }
 
     public override void ExitState(PlayerStateManager player)
     {
         player.rb.useGravity = true;
+    }
+    
+    //util function from chatGPT :)
+    public static Vector3 ClosestPointOnLineSegment(Vector3 start, Vector3 end, Vector3 point)
+    {
+        // Calculate the vector representing the line segment
+        Vector3 line = end - start;
+
+        // Calculate the vector from the start point to the third point
+        Vector3 pointStart = point - start;
+
+        // Calculate the projection of pointStart onto the line segment
+        float projection = Vector3.Dot(pointStart, line) / line.sqrMagnitude;
+
+        // If the projection is less than zero, the closest point is the start point
+        if (projection < 0)
+        {
+            return start;
+        }
+
+        // If the projection is greater than one, the closest point is the end point
+        if (projection > 1)
+        {
+            return end;
+        }
+
+        // Otherwise, the closest point is the projection of the third point onto the line segment
+        Vector3 closestPoint = start + projection * line;
+
+        return closestPoint;
     }
     
 }
