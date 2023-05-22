c2e07fedb50426028ce9914ebec59424949c1f21
Soma Nyiro <nysoma@protonmail.com>
2023-05-22 14:27:00 +0200

    added handles to climbsurface editor

--- Assets/Scripts/Editor/ClimbSurfaceEditor.cs
+++ Assets/Scripts/Editor/ClimbSurfaceEditor.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -18,15 +19,44 @@ public class ClimbSurfaceEditor : Editor
 
         if (GUILayout.Button("Apply"))
         {
-            Transform collision = climbSurface.Collision;
-            Vector3 startPoint = climbSurface.startPoint.position;
-            Vector3 endPoint = climbSurface.endPoint.position;
-
-            collision.position = (startPoint + endPoint) / 2;
-            collision.LookAt(endPoint);
-            collision.localScale = new Vector3(1, 1, Vector3.Distance(startPoint, endPoint));
-            
+            Apply();
         }
+    }
+
+    public void OnSceneGUI()
+    {
+        if (climbSurface is null)
+            return;
+        
+        StartEndHandles();
+    }
+    
+    void StartEndHandles()
+    {
+        Transform handleTransform = climbSurface.transform;
+
+        EditorGUI.BeginChangeCheck();
+        Vector3 newStartPos = Handles.PositionHandle(climbSurface.startPoint.position, Quaternion.identity);
+        Vector3 newEndPos = Handles.PositionHandle(climbSurface.endPoint.position, Quaternion.identity);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(climbSurface, "changed start or end position");
+            EditorUtility.SetDirty(climbSurface);
+            climbSurface.startPoint.position = newStartPos;
+            climbSurface.endPoint.position = newEndPos;
+            Apply();
+        }
+    }
+
+    void Apply()
+    {
+        Transform collision = climbSurface.Collision;
+        Vector3 startPoint = climbSurface.startPoint.position;
+        Vector3 endPoint = climbSurface.endPoint.position;
+
+        collision.position = (startPoint + endPoint) / 2;
+        collision.LookAt(endPoint);
+        collision.localScale = new Vector3(1, 1, Vector3.Distance(startPoint, endPoint));
     }
     
 }
