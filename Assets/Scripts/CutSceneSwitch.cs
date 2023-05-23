8865979163131247c96ede363a93d862cb3b793f
NataliyaSviridenko <516472@student.saxion.nl>
2023-05-23 16:10:05 +0200

    added scene switch

--- Assets/Scripts/CutSceneSwitch.cs
+++ Assets/Scripts/CutSceneSwitch.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
+
 
 public class CutSceneSwitch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string TemplateScene;
+    public TimelineAsset timeline;
+    private bool isHoldingA;
+    private bool isTimelineFinished;
+    [SerializeField] private float timer;
+    [SerializeField] private float timeDelay = 3f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            timer = 0f;
+        }
+        // Check if the "A" key is being held down
+        if (Input.GetKey(KeyCode.A))
+        {
+            //add Time.deltaTime to the timer value
+            timer += Time.deltaTime;
+
+            //check if the timer value is above a certain amount
+            if(timer >= timeDelay)
+            {
+                SwitchToNextScene();
+            }
+        }
+
+
     }
+
+    public void SwitchToNextScene()
+    {
+        // Load the next scene by name
+        //SceneManager.LoadScene(TemplateScene);
+        SceneManager.LoadScene(2);
+    }
+
 }
